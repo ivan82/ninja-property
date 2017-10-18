5dd33adc777a4575f741d66bd788bbe38fc594b2
Ivan Jakesevic <ivan@Ivans-MBP.lan>
2017-10-18 23:26:00 +0200

    no message

---
+++ ninja-property.js
@@ -0,0 +1,47 @@
+var NinjaProperty = {
+	value: function(object, propertyPath){
+		var propertyValueObject = this.valueObject(object, propertyPath);
+		return propertyValueObject.success ? propertyValueObject.value : undefined;
+	},
+
+	valueObject: function(object, propertyPath){
+		var pathArray = this.getPathArray(propertyPath);
+		if(!pathArray || pathArray.length === 0){ return undefined; }
+		return this.valueObjectByPathArray(object, pathArray);
+	},
+
+	/*
+	returns property object: { success: [true|false], value: [the value of the prop|the new value] }
+	*/
+	valueObjectByPathArray: function(object, pathArray, newValue){
+		if(!object){ object = this; }
+		if(!pathArray){ return; }
+
+		for(var i = 0, len = pathArray.length; i < len; i++){
+			if(newValue !== undefined && i === len - 1){
+				object[pathArray[i]] = newValue;
+				return {success: true, value: newValue};
+			}
+			object = object[pathArray[i]];
+
+			//we havent reached the end of the object pathArray, the property name is not defined
+			if(object === undefined && i !== len){
+				return { success: false, value: undefined };
+			}
+		}
+		return { success: true, value: object };
+	},
+
+	getPathArray: function(propertyPath){
+		var pathArray = propertyPath.match(/\w+/g);
+		if(!pathArray){ return; }
+
+		//fixes the integers in the path array
+		var i = pathArray.length;
+		while(i--){
+			if(!isNaN(pathArray[i])){ pathArray[i] = parseInt(pathArray[i]); }
+		}
+
+		return pathArray;
+	}
+};
